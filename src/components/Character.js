6ca5ef52210fb60ea76e0faedec6bb5de53a50c0
Lauren Goss <laurenlgoss98@gmail.com>
2021-12-01 15:22:08 -0500

    added components

---
+++ src/components/Character.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Character() {
+  return null;
+}
+
+export default Character;
---
+++ src/components/HealthBar.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function HealthBar() {
+  return null;
+}
+
+export default HealthBar;
